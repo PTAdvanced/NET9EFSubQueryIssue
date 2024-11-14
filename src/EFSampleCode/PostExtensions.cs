3a956d1f94ebeb35c2febbb56c255bc4e7dbec53
PTAdvanced <ptaylor@advancedco.com>
2024-11-14 23:02:23 +0000

    Sample code with EF query using SubQuery which references an ExtensionMethod

---
+++ src/EFSampleCode/PostExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace EFSampleCode
+{
+    internal static class PostExtensions
+    {
+        internal static IQueryable<Post> ExcludeDeleted(this IQueryable<Post> postsQuery)
+        {
+            return postsQuery.Where(p => !p.IsDeleted);
+        }
+    }
+}
---
+++ src/EFSampleCode/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+using EFSampleCode;
+
+using var db = new BloggingContext();
+
+// Note: This sample requires the database to be created before running.
+Console.WriteLine($"Database path: {db.DbPath}.");
+
+// Read
+Console.WriteLine("Querying with a sub query referencing an extension method");
+var exampleQuery = db.Blogs.Join(
+    db.Posts.ExcludeDeleted(),
+    blog => blog.BlogId,
+    post => post.BlogId,
+    (blog, post) => new { blog, post })
+    .ToList();
+
+Console.WriteLine($"Number results: {exampleQuery.Count}");
